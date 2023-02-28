f628571e0224a17d6b2183c7b77f52e0f7a18148
Frah01 <vinario.francesco@gmail.com>
2023-02-28 16:01:24 +0100

    Aggiunti post

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,19 +3,33 @@
 @section('content')
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
+        {{ __('I tuoi Progetti') }}
     </h2>
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header">{{ __('User Projects') }}</div>
                 <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-                    {{ __('You are logged in!') }}
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Titolo</th>
+                            <th scope="col">Contenuto</th>
+                            <th scope="col">Nome</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($projects as $project)
+                            <tr>
+                              <th scope="row">{{$project['id']}}</th>
+                              <td>{{$project['title']}}</td>
+                              <td>{{$project['content']}}</td>
+                              <td>{{$project['slug']}}</td>
+                            </tr>    
+                            @endforeach
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
